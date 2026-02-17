eb57f512232edc04519221dce5b0be1e622d00a5
saditcvh-dev <saditcvh@gmail.com>
2026-02-16 21:39:35 -0600

    cambios en el servidor

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -16,8 +16,8 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
     task_track_started=True,
-    task_time_limit=30 * 60,  # 30 minutos
-    task_soft_time_limit=25 * 60,  # 25 minutos
+    task_time_limit=2 * 60 * 60 + 60,  # 2 horas + 1 min
+    task_soft_time_limit=2 * 60 * 60,  # 2 horas
     broker_connection_retry_on_startup=True,
     broker_connection_retry=True,
     worker_prefetch_multiplier=1,  # Un task a la vez por worker
